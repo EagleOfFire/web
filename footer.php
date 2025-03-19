f06e94f66d1d4985b0567634eb5ea974f601dbba
Tom FAULQUE <tometjerry83200@gmail.com>
2025-03-19 14:53:15 +0100

    tien maxime

--- footer.php
+++ footer.php
@@ -2,8 +2,3 @@
         <p>&copy; 2025 SeaFight - Tous droits réservés.</p>
 </footer>
 
-<?php if (isset($_COOKIE["dernier_page"])) {
-    echo "Dernière page visitée : " . htmlspecialchars($_COOKIE["dernier_page"]);
-} else {
-    echo "Aucune page consultée récemment.";
-}?>
